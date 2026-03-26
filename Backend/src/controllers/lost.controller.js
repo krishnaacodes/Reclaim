a5a82d95015044b86d4eacbafbd4d0b991e103a0
Krishna Upadhyay <krishnkant.sjp2004@gmail.com>
2026-03-27 01:43:39 +0530

    feat: add setAsRecovered endpoint to mark lost items as recovered

--- Backend/src/controllers/lost.controller.js
+++ Backend/src/controllers/lost.controller.js
@@ -27,9 +27,40 @@ const createLostItem = asyncHandler(async (req,res)=>{
     res.status(200).json({
         createitem
     })
+});
+
+const setAsRecovered = asyncHandler(async (req,res)=>{
+    const id = req.params.id;
+
+
+    const lostItem = await LostItem.findById(id);
+    if(!lostItem){
+      res.status(404).send("Item not found");     
+      return;
+    }
+    const userId = req.user._id;
+    const lostItemUserID = lostItem.user;
+
+    if(!lostItemUserID.equals(userId)){
+         res.status(403).send("You do not have permission to update this item");
+         return;
+    }
+
+
+
+    lostItem.status = "recovered";
+    await lostItem.save();
+
+
+    res.status(200).send("mark as recovered");
+
 })
 
-export {createLostItem};
+
+
+
+
+export {createLostItem,setAsRecovered};
 
 
 
--- Backend/src/routes/lost.route.js
+++ Backend/src/routes/lost.route.js
@@ -1,11 +1,12 @@
 import express from "express";
-import { createLostItem } from "../controllers/lost.controller.js";
-import { authenticateuser } from "../middleware/auth.user.js";
+import { createLostItem , setAsRecovered  } from "../controllers/lost.controller.js";
+import { authenticateuser} from "../middleware/auth.user.js";
 
 const Router = express.Router();
 
 
 Router.post("/createLostItem",authenticateuser,createLostItem);
+Router.post("/setAsRecovered/:id",authenticateuser,setAsRecovered);
 
 
 

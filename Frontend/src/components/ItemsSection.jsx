2c38f3ebaf8429f3bebd35fce6e5d31ef20aa789
Krishna Upadhyay <krishnkant.sjp2004@gmail.com>
2026-05-08 23:54:49 +0530

    kls

--- Frontend/src/components/ItemsSection.jsx
+++ Frontend/src/components/ItemsSection.jsx
@@ -8,20 +8,20 @@ function ItemsSection(){
     let locations = ["IIPS","SCSIT","ICH","Canteen","Temple","Football Ground"];
 
     return (
-        <section className="border border-red-500 max-w-7xl h-200 text-white bg-[#0F0E0D] py-2">
+        <section className=" max-w-7xl text-white  bg-[#0F0E0D] py-2">
             <div className="flex flex-row px-6">
-                <div className="flex flex-col">
+                <div className="flex flex-col  w-50 px-3">
 
-                    <div className="flex flex-col gap-1 pt-5">
-                        <p>STATUS</p>
-                          <div className="flex flex-col gap-1 mt-3 px-3">
+                    <div className="flex flex-col gap-1 pt-1">
+                        <p className="text-[13px]">STATUS</p>
+                          <div className="flex flex-col gap-2 mt-3 px-3">
                             <span>All items</span>
                             <span>Lost</span>
                             <span>Found</span>
                           </div>
                     </div>
                     <div className="flex flex-col gap-1 pt-5">
-                        <p> CATEGORY</p>
+                        <p className="text-[13px]"> CATEGORY</p>
                         <div className="flex flex-col gap-1 mt-3 px-3">
                             {
                                 category.map((e)=>{
@@ -33,7 +33,7 @@ function ItemsSection(){
                         </div>
                     </div>
                     <div className="flex flex-col gap-1 pt-5">
-                        <p>LOCATION</p>
+                        <p className="text-[13px]">LOCATION</p>
                         <div className="flex flex-col gap-1 mt-3 px-3">
                             {
 
@@ -48,7 +48,15 @@ function ItemsSection(){
                     </div>
 
                 </div>
-                <div>
+                <div className=" px-10 w-full "> 
+                    <div className="flex flex-row justify-between w-full ">
+                        <p className="text-xl ">All Items</p>
+                        <select class="border h-8 rounded-lg text-white">
+                          <option className="text-white bg-black">Newest First</option>
+                          <option className="text-white bg-black">Oldest First</option>
+                          <option className="text-white bg-black">Most Viewed</option>
+                        </select>
+                    </div>
 
                 </div>
             </div>

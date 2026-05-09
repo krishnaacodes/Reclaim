e559bd5812d9819850c152736b3218d6bb665832
Krishna Upadhyay <krishnkant.sjp2004@gmail.com>
2026-05-09 10:36:19 +0530

    lsk

---
+++ Frontend/src/components/ItemCard.jsx
@@ -0,0 +1,29 @@
+
+//  <div className=" bg-transparent border border-orange-400 px-4 py-4 rounded-xl">
+//                             <img src="https://imgs.search.brave.com/lcC_TfnW3N9UTFyW4O_g1-7Q5xxfFcMEoHVtJqi9Ix8/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9pMC53/cC5jb20vcGljanVt/Ym8uY29tL3dwLWNv/bnRlbnQvdXBsb2Fk/cy9taW5pbWFsaXN0/LWhlYWRlci1waG90/by13aXRoLXdvbWFu/LXdpdGgtY2xvc2Vk/LWV5ZXMtZnJlZS1p/bWFnZS5qcGVnP3c9/NjAwJnF1YWxpdHk9/ODA"
+//                                 alt="not found" className="rounded-xl h-50"></img>
+//                             <p className="text-xl text-white"> smart watch</p>
+//                             <p className="text-gray-500 text-xl">a black smart watch with lord shiva wallpaper</p>
+//                             <ul className="flex flex-row gap-5 mt-2 ">
+//                                 <li className="text-gray-400">iips</li>
+//                                 <li className="text-gray-400">electronics</li>
+//                             </ul>
+//                             <div className="flex flex-row gap-3 mt-2">
+//                                 <button className="px-2 py-1 bg-white
+//                           text-black rounded-lg hover:bg-gray-200
+//                           transition duration-300">View Details</button>
+//                                 <button className="px-6 py-1 bg-white
+//                           text-black rounded-lg hover:bg-gray-200
+//                           transition duration-300">Claim</button>
+//                             </div>
+//                         </div>
+
+
+
+function ItemCard(){
+    return(
+        <div>
+
+        </div>
+    )
+}
--- Frontend/src/components/ItemsSection.jsx
+++ Frontend/src/components/ItemsSection.jsx
@@ -77,24 +77,7 @@ function ItemsSection() {
                           transition duration-300">Claim</button>
                             </div>
                         </div>
-                        <div className=" bg-transparent border border-orange-400 px-4 py-4 rounded-xl">
-                            <img src="https://imgs.search.brave.com/lcC_TfnW3N9UTFyW4O_g1-7Q5xxfFcMEoHVtJqi9Ix8/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9pMC53/cC5jb20vcGljanVt/Ym8uY29tL3dwLWNv/bnRlbnQvdXBsb2Fk/cy9taW5pbWFsaXN0/LWhlYWRlci1waG90/by13aXRoLXdvbWFu/LXdpdGgtY2xvc2Vk/LWV5ZXMtZnJlZS1p/bWFnZS5qcGVnP3c9/NjAwJnF1YWxpdHk9/ODA"
-                                alt="not found" className="rounded-xl h-50"></img>
-                            <p className="text-xl text-white"> smart watch</p>
-                            <p className="text-gray-500 text-xl">a black smart watch with lord shiva wallpaper</p>
-                            <ul className="flex flex-row gap-5 mt-2 ">
-                                <li className="text-gray-400">iips</li>
-                                <li className="text-gray-400">electronics</li>
-                            </ul>
-                            <div className="flex flex-row gap-3 mt-2">
-                                <button className="px-2 py-1 bg-white
-                          text-black rounded-lg hover:bg-gray-200
-                          transition duration-300">View Details</button>
-                                <button className="px-6 py-1 bg-white
-                          text-black rounded-lg hover:bg-gray-200
-                          transition duration-300">Claim</button>
-                            </div>
-                        </div>
+                       
                         <div className=" bg-transparent border border-orange-400 px-4 py-4 rounded-xl">
                             <img src="https://imgs.search.brave.com/lcC_TfnW3N9UTFyW4O_g1-7Q5xxfFcMEoHVtJqi9Ix8/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9pMC53/cC5jb20vcGljanVt/Ym8uY29tL3dwLWNv/bnRlbnQvdXBsb2Fk/cy9taW5pbWFsaXN0/LWhlYWRlci1waG90/by13aXRoLXdvbWFu/LXdpdGgtY2xvc2Vk/LWV5ZXMtZnJlZS1p/bWFnZS5qcGVnP3c9/NjAwJnF1YWxpdHk9/ODA"
                                 alt="not found" className="rounded-xl h-50"></img>
@@ -185,9 +168,12 @@ function ItemsSection() {
                           transition duration-300">Claim</button>
                             </div>
                         </div>
-                        <div className=" bg-transparent border border-orange-400 px-4 py-4 rounded-xl">
+                        <div className=" relative bg-transparent border border-orange-400 px-4 py-4 rounded-xl">
+                             <p className=" z-2 absolute top-5 left-5 text-red-600  rounded-full font-semibold text-xl w-fit">
+    Lost
+  </p>
                             <img src="https://imgs.search.brave.com/lcC_TfnW3N9UTFyW4O_g1-7Q5xxfFcMEoHVtJqi9Ix8/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9pMC53/cC5jb20vcGljanVt/Ym8uY29tL3dwLWNv/bnRlbnQvdXBsb2Fk/cy9taW5pbWFsaXN0/LWhlYWRlci1waG90/by13aXRoLXdvbWFu/LXdpdGgtY2xvc2Vk/LWV5ZXMtZnJlZS1p/bWFnZS5qcGVnP3c9/NjAwJnF1YWxpdHk9/ODA"
-                                alt="not found" className="rounded-xl h-50"></img>
+                                alt="not found" className="rounded-xl h-50 z-1"></img>
                             <p className="text-xl text-white"> smart watch</p>
                             <p className="text-gray-500 text-xl">a black smart watch with lord shiva wallpaper</p>
                             <ul className="flex flex-row gap-5 mt-2 ">
